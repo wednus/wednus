d2e2b2a00d876800be057a342e47668004f6fa8c
sundew@wednus.com <sundew@wednus.com@ef846854-8cc2-11dd-84d8-5ff37064ad4b>
2008-09-28 04:53:49 +0000

    initial submission

---
+++ trunk/wednus_platform/module/dev/dev_plus.js
@@ -0,0 +1,12 @@
+/**
+ * @defgroup    dev    dev, Wednus Event System
+ * @ingroup dev
+ *  The implementation of the dev, Wednus Dev. System.
+ * @{
+ */
+
+//alert('you are in.');
+
+/**
+ * @}  // end of dev
+ */
